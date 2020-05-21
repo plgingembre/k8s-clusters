92494b61a77c4018794f0c1c497cf84e7634cc8f
Pierre-Louis Gingembre <pierre-louisgingembre@SV-LT-1695.local>
2020-05-21 12:07:25 -0700

    Added a test.sh file. Modified create_6_nodes_cluster.sh

--- test.sh
+++ test.sh
@@ -18,10 +18,19 @@ echo "Cluster ID: $CLUSTER_ID"
 # Node preparation in AWS
 aws cloudformation create-stack --stack-name k8s-tests-$CLUSTER_ID --template-body file://aws-cloudformation/6-nodes-cluster.json --parameters ParameterKey=SSHKey,ParameterValue=aws_demo_sales_new ParameterKey=TestClusterID,ParameterValue=$CLUSTER_ID
 
-# Waiting for CloudFormation to be done
-until [ -z "$(aws cloudformation list-stack-resources --stack-name=k8s-tests-$CLUSTER_ID | grep ResourceStatus | grep -v "CREATE_COMPLETE")" ]; do
+until [ -z $(aws cloudformation list-stack-resources --stack-name=k8s-tests-$CLUSTER_ID | grep ResourceStatus | grep -v "CREATE_COMPLETE") ]; do
   echo "Automation is running......"
   sleep 5s
+  #if [ $(aws ssm get-automation-execution --automation-execution-id "$id" --query 'AutomationExecution.AutomationExecutionStatus' --output text) != "InProgress" ]; then
+  #   echo "Automation Finished"
+  #   status=$(aws ssm get-automation-execution --automation-execution-id "$id" --query 'AutomationExecution.AutomationExecutionStatus' --output text)
+  #   echo "Automation $status"
+  #   if [$status != "Success"]; then
+  #      exit 3
+  #      echo "Automation $status"
+  #   fi   
+  #  break
+  #fi
 done
 
 # Master nodes
@@ -41,38 +50,5 @@ echo "WORKER1_IP is $WORKER1_IP"
 echo "WORKER2_IP is $WORKER2_IP"
 echo "WORKER3_IP is $WORKER3_IP"
 
-cd kubespray
-
 echo ""
-echo "===> Creating a new folder for cluster-$CLUSTER_ID"
-cp -rpvf inventory/template-1/ inventory/cluster-$CLUSTER_ID/
-
-echo ""
-echo "===> Creating the list of nodes for cluster-$CLUSTER_ID"
-declare -a NODES=(master1,$MASTER1_IP master2,$MASTER2_IP master3,$MASTER3_IP worker1,$WORKER1_IP worker2,$WORKER2_IP worker3,$WORKER3_IP)
-echo "===> List of nodes: ${NODES[@]}"
-
-echo ""
-echo "===> Creating an inventory file for cluster-$CLUSTER_ID"
-CONFIG_FILE=inventory/cluster-$CLUSTER_ID/hosts.yml python3 contrib/inventory_builder/inventory.py ${NODES[@]}
-echo "===> Inventory file:"
-cat inventory/cluster-$CLUSTER_ID/hosts.yml
-
-#echo ""
-#echo "===> Removing worker-1 as part of the master nodes in the inventory"
-#sed '19d' inventory/cluster-$CLUSTER_ID/hosts.yml > inventory/cluster-$CLUSTER_ID/inventory.yml
-#rm inventory/cluster-$CLUSTER_ID/hosts.yml
-
-#echo ""
-#echo "===> New inventory file:"
-#cat inventory/cluster-$CLUSTER_ID/inventory.yml
-
-echo ""
-echo "===> Replacing "
-sed -i 's/cluster-X/cluster-'${CLUSTER_ID}'/g' inventory/cluster-$CLUSTER_ID/group_vars/k8s-cluster/k8s-cluster.yml
-echo "===> Checking result in the file"
-cat inventory/cluster-$CLUSTER_ID/group_vars/k8s-cluster/k8s-cluster.yml | grep cluster-
-
-echo ""
-echo "===> Installing Kubernetes"
-#ansible-playbook -i inventory/cluster-$CLUSTER_ID/hosts.yml --become --become-user=root cluster.yml
+echo "Done!"
