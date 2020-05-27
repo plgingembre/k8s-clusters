39a34a75a2e4a47ac64c3badc3d4366329531b5e
Ubuntu <ubuntu@ansible-server.illumio-demo.com>
2020-05-27 06:56:47 +0000

    added new files

--- create_cluster.sh
+++ create_cluster.sh
@@ -11,16 +11,29 @@ echo ""
 echo "===> Plese provide the cluster information:"
 read -p "Enter the cluster ID: " CLUSTER_ID
 read -p "Enter the number of master nodes: " MASTER_NODES
+read -p "Enter the node OS [centos/ubuntu]: " NODE_OS
 
-echo ""
-echo "===> Results of your variables:"
-echo "Cluster ID: $CLUSTER_ID"
-echo "Number of Master nodes: $MASTER_NODES"
+#echo ""
+#echo "===> Results of your variables:"
+#echo "Cluster ID: $CLUSTER_ID"
+#echo "Number of Master nodes: $MASTER_NODES"
 
 # Node preparation in AWS
 echo ""
-echo "===> Creating a CloudFormation stack in AWS named k8s-tests-$CLUSTER_ID"
-aws cloudformation create-stack --stack-name k8s-tests-$CLUSTER_ID --template-body file://aws-cloudformation/6-nodes-centos-7-8-v1.json --parameters ParameterKey=SSHKey,ParameterValue=aws_demo_sales_new ParameterKey=TestClusterID,ParameterValue=$CLUSTER_ID
+case "$NODE_OS" in
+  centos)
+    echo "===> Creating a CloudFormation stack in AWS named k8s-tests-$CLUSTER_ID"
+    aws cloudformation create-stack --stack-name k8s-tests-$CLUSTER_ID --template-body file://aws-cloudformation/6-nodes-centos-7-8-v1.json --parameters ParameterKey=SSHKey,ParameterValue=aws_demo_sales_new ParameterKey=TestClusterID,ParameterValue=$CLUSTER_ID
+    ;;
+  ubuntu)
+    echo "===> Creating a CloudFormation stack in AWS named k8s-tests-$CLUSTER_ID"
+    aws cloudformation create-stack --stack-name k8s-tests-$CLUSTER_ID --template-body file://aws-cloudformation/6-nodes-ubuntu-18-04-v2.json --parameters ParameterKey=SSHKey,ParameterValue=aws_demo_sales_new ParameterKey=TestClusterID,ParameterValue=$CLUSTER_ID
+    ;;
+  *)
+    echo "You did not select a supported node OS. Exiting Now."
+    exit 1
+    ;;
+esac
 
 # Waiting for CloudFormation to be done
 echo ""
@@ -39,16 +52,16 @@ WORKER1_IP=$(nslookup c${CLUSTER_ID}w1 | grep Address | awk 'END { print }' | se
 WORKER2_IP=$(nslookup c${CLUSTER_ID}w2 | grep Address | awk 'END { print }' | sed s'/Address: //g')
 WORKER3_IP=$(nslookup c${CLUSTER_ID}w3 | grep Address | awk 'END { print }' | sed s'/Address: //g')
 
-echo ""
-echo "List of IP addresses for this cluster:"
-# Master nodes IP addresses
-echo "MASTER1_IP is $MASTER1_IP"
-echo "MASTER2_IP is $MASTER2_IP"
-echo "MASTER3_IP is $MASTER3_IP"
-# Worker nodes IP addresses
-echo "WORKER1_IP is $WORKER1_IP"
-echo "WORKER2_IP is $WORKER2_IP"
-echo "WORKER3_IP is $WORKER3_IP"
+#echo ""
+#echo "List of IP addresses for this cluster:"
+## Master nodes IP addresses
+#echo "MASTER1_IP is $MASTER1_IP"
+#echo "MASTER2_IP is $MASTER2_IP"
+#echo "MASTER3_IP is $MASTER3_IP"
+## Worker nodes IP addresses
+#echo "WORKER1_IP is $WORKER1_IP"
+#echo "WORKER2_IP is $WORKER2_IP"
+#echo "WORKER3_IP is $WORKER3_IP"
 
 cd kubespray
 
@@ -71,7 +84,7 @@ echo "===> Creating an inventory file for cluster-$CLUSTER_ID"
 #echo "Debug - CONFIG_FILE is $CONFIG_FILE"
 # Executing script to create the hosts inventory for ansible
 #HOST_PREFIX="blah" KUBE_MASTERS_MASTERS="3" CONFIG_FILE=inventory/cluster-$CLUSTER_ID/hosts.yml python3 contrib/inventory_builder/inventory.py ${NODES[@]}
-KUBE_MASTERS_MASTERS="$MASTER_NODES" CONFIG_FILE=inventory/cluster-$CLUSTER_ID/hosts.yml python3 contrib/inventory_builder/inventory.py ${NODES[@]}
+HOST_PREFIX="c${CLUSTER_ID}node" KUBE_MASTERS_MASTERS="$MASTER_NODES" CONFIG_FILE=inventory/cluster-$CLUSTER_ID/hosts.yml python3 contrib/inventory_builder/inventory.py ${NODES[@]}
 echo ""
 echo "===> Inventory file:"
 cat inventory/cluster-$CLUSTER_ID/hosts.yml
